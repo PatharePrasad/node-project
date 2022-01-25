7533a318b6f3c878c5a07742194b261e0e42c0d2
prasad373 <prasadsachinist10@gmail.com>
2022-01-25 23:17:35 +0530

    model not defined. Error solved

--- model/User.js
+++ model/User.js
@@ -20,5 +20,5 @@ const UserSchema = new Schema({
         default: Date.now
     },
 });
-const User = model('user', UserSchema);
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
