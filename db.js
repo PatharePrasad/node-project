e8c2097d1840d8d6b861b6a863083a93ff4d8d70
prasad373 <prasadsachinist10@gmail.com>
2022-01-27 23:05:43 +0530

    doing one change

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = process.env.mongodb_URI
+const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
